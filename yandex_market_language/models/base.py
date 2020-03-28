3c08a7e2d1853dfef0cc76b5502e31694048a9e2
Alexandr <stefanitsky.mozdor@gmail.com>
2020-03-28 18:32:50 +0300

    :sparkles: added xml tree annotations / to_dict & to_xml methods

--- yandex_market_language/models/base.py
+++ yandex_market_language/models/base.py
@@ -1,5 +1,16 @@
-from abc import ABC
+from abc import ABC, abstractmethod
+from xml.etree import ElementTree as ET
+
+
+XMLElement = ET.Element
+XMLSubElement = ET.SubElement
 
 
 class BaseModel(ABC):
-    pass
+    @abstractmethod
+    def to_dict(self) -> dict:
+        pass
+
+    @abstractmethod
+    def to_xml(self, root_el: XMLElement = None) -> XMLElement:
+        pass
