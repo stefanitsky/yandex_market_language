edc734ef2edbcb7cc476268947682b2bd4d3791c
Alexandr <stefanitsky.mozdor@gmail.com>
2020-03-28 16:57:07 +0300

    :seedling: added parser module with a basic implementation

---
+++ yandex_market_language/parser.py
@@ -0,0 +1,12 @@
+from xml.etree import ElementTree as ET
+
+from yandex_market_language.models import Feed
+
+
+class YMLParser:
+    def __init__(self, file_or_path):
+        self._file_or_path = file_or_path
+        self._tree = ET.parse(file_or_path)
+
+    def parse(self) -> Feed:
+        return Feed()
