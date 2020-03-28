b15c8da0b5201261fcafb7fc921822a81bb0086a
Alexandr <stefanitsky.mozdor@gmail.com>
2020-03-28 18:31:54 +0300

    :white_check_mark: added Feed model test case

---
+++ tests/test_models.py
@@ -0,0 +1,22 @@
+from unittest import TestCase
+from xml.etree import ElementTree as ET
+from yandex_market_language import models
+
+
+class FeedModelTestCase(TestCase):
+    def test_feed_model_to_dict(self):
+        shop = models.Shop("test")
+        feed = models.Feed(shop)
+        feed_dict = feed.to_dict()
+        self.assertEqual(list(feed_dict.keys()), ["shop", "date"])
+        self.assertEqual(feed_dict["date"], feed.date)
+        self.assertEqual(feed_dict["shop"], feed.shop.to_dict())
+
+    def test_feed_model_to_xml(self):
+        shop = models.Shop("test")
+        feed = models.Feed(shop)
+        feed_el = feed.to_xml()
+        self.assertEqual(list(el.tag for el in feed_el), ["shop"])
+        self.assertEqual(ET.tostring(feed_el[0]), ET.tostring(shop.to_xml()))
+        self.assertEqual(feed_el.tag, "yml_catalog")
+        self.assertEqual(feed_el.get("date"), feed.date)
