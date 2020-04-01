8d854a29cdc4c004ed66869a780d4512a6b5dc65
Alexandr <stefanitsky.mozdor@gmail.com>
2020-04-01 17:42:58 +0300

    :white_check_mark: add recursively test to compare source & created xml

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,4 +1,6 @@
 import os
+import re
+from operator import attrgetter
 
 from xml.etree import ElementTree as ET
 from unittest import TestCase
@@ -12,12 +14,50 @@ VALID_XML_PATH = os.path.join(
     "fixtures/valid_feed.xml"
 )
 
+pattern = re.compile(r"\s+")
+
+
+def clean_element_text(el: ET.Element):
+    """
+    Remove whitespaces, new lines & tabs from element text.
+    """
+    if el.text:
+        el.text = str(el.text)
+        el.text = re.sub(pattern, "", el.text)
+    else:
+        el.text = ""
+
 
 class YMLParserTestCase(TestCase):
+    def assertElementsEquals(self, el1, el2):
+        clean_element_text(el1)
+        clean_element_text(el2)
+        self.assertEqual(el1.text, el2.text)
+        self.assertEqual(el1.attrib, el2.attrib)
+
+    def compare_elements(self, el1, el2):
+        self.assertElementsEquals(el1, el2)
+
+        # Debug message
+        print("SUCCESS COMPARE: {0} == {1}".format(el1.tag, el2.tag))
+        if el1.tag == "offer":
+            print("ENTERED IN OFFER: ", el1.attrib["id"])
+
+        # Sort elements by key
+        el1[:] = sorted(el1, key=attrgetter("tag"))
+        el2[:] = sorted(el2, key=attrgetter("tag"))
+
+        # Call compare recursively
+        for el1_, el2_ in zip(el1, el2):
+            self.assertEqual(el1_.tag, el2_.tag)
+            self.compare_elements(el1_, el2_)
+
     def test_parser_converts_valid_xml(self):
         p = YMLParser(VALID_XML_PATH)
         feed = p.parse()
-        xml_feed = ET.tostring(ET.parse(VALID_XML_PATH).getroot())
-        expected_xml_feed = ET.tostring(feed.to_xml())
+
+        source_xml = ET.parse(VALID_XML_PATH).getroot()
+        expected_xml = feed.to_xml()
+
         self.assertIsInstance(feed, models.Feed)
-        self.assertEqual(xml_feed, expected_xml_feed)
+        self.compare_elements(source_xml, expected_xml)
