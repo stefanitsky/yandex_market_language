67eb569d336a5f2687524799c0a61e5ed6e9d906
Alexandr <stefanitsky.mozdor@gmail.com>
2020-04-01 17:50:23 +0300

    :alembic: added commented (for a while) prettify method for xml

--- yandex_market_language/parser.py
+++ yandex_market_language/parser.py
@@ -1,3 +1,4 @@
+# from xml.dom import minidom
 from xml.etree import ElementTree as ET
 
 from yandex_market_language.models import Feed
@@ -6,6 +7,15 @@ from yandex_market_language.models import Feed
 class YMLParser:
     def __init__(self, file_or_path):
         self._file_or_path = file_or_path
+
+    # @staticmethod
+    # def prettify(el: ET.Element) -> str:
+    #     """
+    #     Return a pretty-printed XML string for the Element.
+    #     """
+    #     raw = ET.tostring(el, "utf-8")
+    #     parsed = minidom.parseString(raw)
+    #     return parsed.toprettyxml(indent="\t")
 
     def parse(self) -> Feed:
         tree = ET.parse(self._file_or_path)
