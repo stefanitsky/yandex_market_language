af969ddb96bff695b3d1090b398619b99431baff
Alexandr Stefanitsky-Mozdor <stefanitsky.mozdor@gmail.com>
2020-04-02 20:03:04 +0300

    🔖 Bump version: 0.4.0 → 0.5.0

--- yandex_market_language/__init__.py
+++ yandex_market_language/__init__.py
@@ -4,4 +4,4 @@ Top-level package for Yandex Market Language (YML) for Python.
 
 __author__ = """Alexandr Stefanitsky-Mozdor"""
 __email__ = "stefanitsky.mozdor@gmail.com"
-__version__ = "0.4.0"
+__version__ = "__version__ = '0.5.0'"
