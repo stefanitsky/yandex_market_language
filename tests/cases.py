5f03c4ae03d3a75a3781f97b3b5cf7a69cafd8a8
Alexandr <stefanitsky.mozdor@gmail.com>
2020-04-02 15:17:21 +0300

    :white_check_mark: added elements sort for assert test

--- tests/cases.py
+++ tests/cases.py
@@ -1,3 +1,4 @@
+from operator import attrgetter
 from unittest import TestCase
 from xml.etree import ElementTree as ET
 from faker import Faker
@@ -7,4 +8,9 @@ fake = Faker()
 
 class ModelTestCase(TestCase):
     def assertElementsEquals(self, el, expected_el):
+        # Sort elements by key
+        el[:] = sorted(el, key=attrgetter("tag"))
+        expected_el[:] = sorted(expected_el, key=attrgetter("tag"))
+
+        # Check if elements are equal
         self.assertEqual(ET.tostring(el), ET.tostring(expected_el))
