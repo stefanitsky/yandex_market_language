78e57f9812fcfa5cab9e6199149dcde663c3cb94
Alexandr <stefanitsky.mozdor@gmail.com>
2020-04-06 12:54:45 +0300

    :seedling: added commented exceptions for future use

--- yandex_market_language/exceptions.py
+++ yandex_market_language/exceptions.py
@@ -7,6 +7,27 @@ class YMLException(Exception):
 
 class ValidationError(YMLException):
     """
-    Error for data validation exceptions for models.
+    Data validation exception.
     """
     pass
+
+
+# class ParseError(YMLException):
+#     """
+#     Base parse exception.
+#     """
+#     pass
+#
+#
+# class UnsupportedField(ParseError):
+#     """
+#     Unsupported field exception for parsing.
+#     """
+#     def __init__(self, cls, field):
+#         self.cls = cls
+#         self.field = field
+#
+#     def __str__(self):
+#         return "{cls} contains unsupported field {field}".format(
+#             cls=self.cls.__name__, field=self.field
+#         )
